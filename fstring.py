748e5695fa34f09a634b9277ac2329d4f6fae8d3
JonieAlaban <joniealaban@gmail.com>
2024-03-23 14:51:58 +0800

    first commit

---
+++ fstring.py
@@ -0,0 +1,8 @@
+#f string will help you to convert the int, float and boleen variables. 
+
+score = 0
+height = 1.8 
+isWinning = True 
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
